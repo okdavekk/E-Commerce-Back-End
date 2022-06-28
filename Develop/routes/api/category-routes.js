7a9932cad8bd39c143300497f4a463a21f972b66
okdavekk <okdavekk@gmail.com>
2022-06-28 17:41:44 -0400

    Added try and catch function to reouter.post and it did not crash nodemon.

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -31,15 +31,21 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const userData = await Category.create(req.body);
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
 });
 
