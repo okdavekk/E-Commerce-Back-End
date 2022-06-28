d68a922316cf9d0849543b2fe0979ddb0c8f4283
okdavekk <okdavekk@gmail.com>
2022-06-28 17:43:31 -0400

    Added try and put stuff to router.put and it did not crash nodemon.

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -43,6 +43,16 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const userData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
